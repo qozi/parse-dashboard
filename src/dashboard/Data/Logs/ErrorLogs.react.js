25d8d75682da199c470e5b4659f5067d8c89c8b9
Natália Condê <natalia.csouza98@outlook.com>
2021-07-30 17:47:51 -0300

    avoid object type

--- src/dashboard/Data/Logs/ErrorLogs.react.js
+++ src/dashboard/Data/Logs/ErrorLogs.react.js
@@ -131,6 +131,7 @@ export default class InfoLogs extends DashboardView {
     if (this.state.loading) {
       refreshIconStyles += ` ${styles.toolbarButtonDisabled}`;
     }
+    const logs = this.state.logs;
     let toolbar = null;
     toolbar = (
       <Toolbar
@@ -155,7 +156,7 @@ export default class InfoLogs extends DashboardView {
     content = (
       <LoaderContainer loading={this.state.loading} solid={false}>
         <div className={styles.content}>
-          {!this.state.loading && this.state.logs.length === 0 && (
+          {!this.state.loading && (!Array.isArray(logs) || logs.length === 0) && (
             <EmptyState
               icon="files-outline"
               title="No Error logs in the last 30 days"
@@ -164,11 +165,11 @@ export default class InfoLogs extends DashboardView {
               action={"https://www.back4app.com/docs/platform/parse-server-logs"}
             />
           )}
-          {!this.state.loading && this.state.logs.length !== 0 && (
+          {!this.state.loading && Array.isArray(logs) && logs.length !== 0 && (
             <div>
               {alertWhatIs}
               <LogView>
-                {this.state.logs.map(({ message, timestamp }) => (
+                {logs.map(({ message, timestamp }) => (
                   <LogViewEntry
                     key={timestamp}
                     text={message}
